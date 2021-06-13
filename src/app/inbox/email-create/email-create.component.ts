b168f49288dc13f67040e5a27277ec46ce8dc727
Slawomir Kaczynski <slawomir.kaczynski@billennium.com>
2021-06-13 20:26:29 +0200

    Add send email funcionality

--- src/app/inbox/email-create/email-create.component.ts
+++ src/app/inbox/email-create/email-create.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "src/app/auth/auth.service";
+import { Email } from "../email";
+import { EmailService } from "../email.service";
 
 @Component({
-  selector: 'app-email-create',
-  templateUrl: './email-create.component.html',
-  styleUrls: ['./email-create.component.css']
+  selector: "app-email-create",
+  templateUrl: "./email-create.component.html",
+  styleUrls: ["./email-create.component.css"],
 })
 export class EmailCreateComponent implements OnInit {
+  showModal = false;
+  email: Email;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private authService: AuthService,
+    private emailService: EmailService
+  ) {
+    this.email = {
+      id: "",
+      to: "",
+      subject: "",
+      html: "",
+      text: "",
+      from: `${this.authService.username}@angular-email.com`,
+    };
   }
 
+  ngOnInit() {}
+
+  onSubmit(email: Email) {
+    this.emailService.sendEmail(email).subscribe(() => {
+      this.showModal = false;
+    });
+  }
 }
