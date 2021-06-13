868963cb0bba307aeb77309674af4499bf398d7f
Slawomir Kaczynski <slawomir.kaczynski@billennium.com>
2021-06-13 13:00:39 +0200

    Add ability to display html emails

--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { switchMap } from "rxjs/operators";
+import { Email } from "../email";
+import { EmailService } from "../email.service";
 
 @Component({
-  selector: 'app-email-show',
-  templateUrl: './email-show.component.html',
-  styleUrls: ['./email-show.component.css']
+  selector: "app-email-show",
+  templateUrl: "./email-show.component.html",
+  styleUrls: ["./email-show.component.css"],
 })
 export class EmailShowComponent implements OnInit {
+  email: Email;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute,
+    private emailService: EmailService
+  ) {
+    this.route.data.subscribe(({email})=> this.email = email)
   }
 
+  ngOnInit() {
+    // this.route.params
+    //   .pipe(
+    //     switchMap(({ id }) => {
+    //       return this.emailService.getEmail(id);
+    //     })
+    //   )
+    //   .subscribe((email) => {
+    //     this.email = email;
+    //   });
+  }
 }
